33af16ef6002a926303861b6ff2117c2d572105b
Stephan Tiebosch <s.tiebosch@deamethyst.nl>
2022-01-19 02:44:47 +0100

    Major changes with regard to wkhtmltopdf container

--- publish_scriptcase.sh
+++ publish_scriptcase.sh
@@ -2,27 +2,47 @@
 #
 # Install automatic by scriptcase generated php applications in production
 # newly created  .tar.gz ,  .tar and .sql files will be published automatically
-# Check is done firstly in ../pureftpd and if not exits in app_install and mysql_install directories 
+# Check is done firstly in ../pureftpd and if not exits in app_install/project[n] and mysql_install directories 
 # Author: Stephan Tiebosch
 #
 
-source .env
+if [ "$1" == "-h" ]; then
+  echo "Usage $0 <no>"
+  echo " <no> is project number coresponds to docker-compose.yml and .env (1) or dc_project<no> (2..n) and .env[n])"
+  exit
+fi
+if [ -z "$1" ]; then
+  no=1;
+else
+  no=$1;
+fi
+if [ "$no" == "1" ]; then
+  source .env
+else
+  source .env${no}
+fi
 
 # if ../pure-ftpd exists
-FILE=${PUREFTPD_DIR}.env
+FILE=${PUREFTPD_DIR}/.env
 if [ -f "$FILE" ]; then
   source $FILE
-  UPDATEDIR=${PUREFTPD_DATA_DIR}/${FTP_USER}
+  UPDATEDIR=${PUREFTPD_DATA_DIR}/${FTP_USER}/project${no}
 else
-  UPDATEDIR=app_install mysql_install 
+  UPDATEDIR=app_install/project${no} mysql_install 
 fi
 
 
-while true; do
-#  inotifywait -e create -e moved_to $UPDATE_DIR
-  publish_app $UPDATE_DIR
-  sleep $DEPLOY_INTERVAL_SEC
-done
+function update_rights() {
+  # correct rights and owner
+  for xd in `find $1 -type f`; do
+    chmod 644 $xd
+  done
+  for xd in `find $1 -type d`; do
+    chmod 755 $xd
+  done
+  chown ${BITNAMI_PHP_USER}:${BITNAMI_PHP_GROUP} ./app/${project} -R
+}
+
 
 
 function publish_app() {
@@ -30,23 +50,25 @@ function publish_app() {
   echo $dir
      project=${dir##*/}
 echo $project
-      for file in find $dir -maxdepth 1 -type f`; do
+      for file in `find $dir -maxdepth 1 -type f`; do
 echo $file
-        if [[ $file == *.tar.gz ]] then
+        if [[ $file == sc*prod*.tar* ]]; then
+          tar xvf $file -C app/${project}/${SCRIPTCASE_PROD_LIB}
+          echo "Publishing scriptcase Prod environment from: $file"
+          echo "Please goto ${project}.domain.com/_lib/prod to check/configure database connection "
+          mv $file app_install/${project}/history 
+          update_rights "app/project${no}/${SCRIPTCASE_PROD_LIB}"
+        fi
+        if [[ $file == *.tar.gz ]]; then
           tar xvfz $file -C app/${project}
           echo "Publishing scriptcase php files from:" $file
-          # Special change for app_Login as it seems redirect is not to working with nginx-proxy
+          # Special change for start application (app_Login) as it seems redirect is not to working with nginx-proxy
           # Maybe myapp_projectx.conf changes can overcome this but is not found yet 
-          echo "app_Login/" > app/project$no/_lib/friendly_url/app_Login_ini.txt
+          rm app/project${no}/_lib/friendly_url/${SCRIPTCASE_STARTAPP}_ini.txt
           mv $file app_install/${project}/history 
+          update_rights "app/project${no}"
         fi
-        if [[ $file == *prod*.tar ]] then
-          tar xvf $file -C app/${project}/${SCRIPTCASE_PROD_LIB}
-          echo "Publishing scriptcase Prod environment from:" $file
-          echo "Please goto ${project}.domain.com/_lib/prod to check/configure database connection" $file
-          mv $file app_install/${project}/history 
-        fi
-        if [[ $file == *.sql ]] then
+        if [[ $file == *.sql ]]; then
 	  # Execute sql file 
           echo Importing in database $file
           IMPORT_COMMAND='exec mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD"'
@@ -54,17 +76,13 @@ echo $file
           mv $file mysql_install/history 
         fi
      done
-echo         rm $file
   done
-  # correct rights and owner
-  for xd in `find ./app -type f`; do
-    chmod 644 $xd
-  done
-  for xd in `find ./app -type d`; do
-    chmod 755 $xd
-  done
-  chown ${BITNAMI_PHP_USER}:${BITNAMI_PHP_GROUP} ./app -R 
-
 }
 
+
+while true; do
+  inotifywait -e create -e moved_to $UPDATE_DIR
+  publish_app $UPDATEDIR
+  sleep $DEPLOY_INTERVAL_SEC
+done
 
