4be9ce2abb04bfe452c7b5a00c2dbeb61e2cb510
Stephan Tiebosch <s.tiebosch@deamethyst.nl>
2022-10-11 15:54:20 +0200

    Support for scriptcase 9.8 and php 8.1.6

--- get_example_files.sh
+++ get_example_files.sh
@@ -4,9 +4,15 @@ if [ -z $1 ]; then
 else
   no=$1
 fi
+source .env
+
+echo "get_examples.sh: only $SCRIPTCASE_VERSION 9.7.009 and 9.8.009 are available"
+
 wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/securityexample.sql'
 mv securityexample.sql mysql_install
-wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/sc_prod_9.7.009.tar'
-mv sc_prod_9.7.009.tar app_install/project${no}
-wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/securityexample_20220123180138.tar.gz'
-mv securityexample_20220123180138.tar.gz app_install/project${no}
+echo wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/sc_prod_'${SCRIPTCASE_VERSION}'.tar'
+wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/sc_prod_'${SCRIPTCASE_VERSION}'.tar'
+mv sc_prod_${SCRIPTCASE_VERSION}.tar app_install/project${no}
+echo wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/securityexample_'${SCRIPTCASE_VERSION}'.tar.gz'
+wget 'https://deam-scriptcase.s3.eu-west-1.amazonaws.com/securityexample/securityexample_'${SCRIPTCASE_VERSION}'.tar.gz'
+mv securityexample_${SCRIPTCASE_VERSION}.tar.gz app_install/project${no}
